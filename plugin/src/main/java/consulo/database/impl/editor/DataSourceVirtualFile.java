64e170b405204048155f629bd03b3613c1b43ade
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-06-02 15:11:17 +0400

    compile fix

--- plugin/src/main/java/consulo/database/impl/editor/DataSourceVirtualFile.java
+++ plugin/src/main/java/consulo/database/impl/editor/DataSourceVirtualFile.java
@@ -20,69 +20,64 @@ import consulo.database.datasource.model.DataSource;
 import consulo.language.file.light.LightVirtualFile;
 import consulo.virtualFileSystem.VirtualFileSystem;
 import consulo.virtualFileSystem.archive.ArchiveFileSystem;
-
 import jakarta.annotation.Nonnull;
 
 /**
  * @author VISTALL
  * @since 2020-08-19
  */
-public class DataSourceVirtualFile extends LightVirtualFile
-{
-	private final DataSource myDataSource;
-	private final String myDatabaseName;
-	private final String myChildId;
-	private final VirtualFileSystem myVirtualFileSystem;
+public class DataSourceVirtualFile extends LightVirtualFile {
+    private final DataSource myDataSource;
+    private final String myDatabaseName;
+    private final String myChildId;
+    private final VirtualFileSystem myVirtualFileSystem;
 
-	public DataSourceVirtualFile(@Nonnull DataSource dataSource, @Nonnull String databaseName, @Nonnull String childId, @Nonnull VirtualFileSystem virtualFileSystem)
-	{
-		super("[" + dataSource.getName() + "] " + childId, DataSourceFileType.INSTANCE, "");
+    public DataSourceVirtualFile(@Nonnull DataSource dataSource, @Nonnull String databaseName, @Nonnull String childId, @Nonnull VirtualFileSystem virtualFileSystem) {
+        super("[" + dataSource.getName() + "] " + childId, DataSourceFileType.INSTANCE, "");
 
-		myDataSource = dataSource;
-		myDatabaseName = databaseName;
-		myChildId = childId;
-		myVirtualFileSystem = virtualFileSystem;
-	}
+        myDataSource = dataSource;
+        myDatabaseName = databaseName;
+        myChildId = childId;
+        myVirtualFileSystem = virtualFileSystem;
+    }
 
-	@Nonnull
-	public String getDatabaseName()
-	{
-		return myDatabaseName;
-	}
+    @Nonnull
+    public String getDatabaseName() {
+        return myDatabaseName;
+    }
 
-	@Nonnull
-	public String getChildId()
-	{
-		return myChildId;
-	}
+    @Nonnull
+    public String getChildId() {
+        return myChildId;
+    }
 
-	@Nonnull
-	public DataSource getDataSource()
-	{
-		return myDataSource;
-	}
+    @Nonnull
+    public DataSource getDataSource() {
+        return myDataSource;
+    }
 
-	@Nonnull
-	@Override
-	public VirtualFileSystem getFileSystem()
-	{
-		return myVirtualFileSystem;
-	}
+    @Nonnull
+    @Override
+    public VirtualFileSystem getFileSystem() {
+        return myVirtualFileSystem;
+    }
 
-	@Nonnull
-	@Override
-	public String getPath()
-	{
-		return myDataSource.getId() + ArchiveFileSystem.ARCHIVE_SEPARATOR + myDatabaseName + "/" + myChildId;
-	}
+    @Nonnull
+    @Override
+    public String getPath() {
+        return myDataSource.getId() + ArchiveFileSystem.ARCHIVE_SEPARATOR + myDatabaseName + "/" + myChildId;
+    }
 
-	@Override
-	public boolean equals(Object obj)
-	{
-		if(obj instanceof DataSourceVirtualFile)
-		{
-			return getPath().equals(((DataSourceVirtualFile) obj).getPath());
-		}
-		return super.equals(obj);
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof DataSourceVirtualFile) {
+            return getPath().equals(((DataSourceVirtualFile) obj).getPath());
+        }
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return getPath().hashCode();
+    }
 }
